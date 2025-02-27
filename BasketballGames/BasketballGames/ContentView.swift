4ba5520a6fc6e3bebd1a0db1e45c531a0eb0b82c
Sam Shi <samrshi@outlook.com>
2025-02-27 18:52:33 -0500

    Sams Implementation

--- BasketballGames/BasketballGames/ContentView.swift
+++ BasketballGames/BasketballGames/ContentView.swift
@@ -7,15 +7,65 @@
 
 import SwiftUI
 
+struct Score: Codable {
+    let unc: Int
+    let opponent: Int
+}
+
+struct BasketballGame: Codable {
+    let id: Int
+    let team: String
+    let date: String
+    let opponent: String
+    let isHomeGame: Bool
+    let score: Score
+}
+
 struct ContentView: View {
+    @State private var games: [BasketballGame] = []
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(games, id: \.id) { game in
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("\(game.team) vs. \(game.opponent)")
+                    
+                    Text("\(game.date)")
+                        .font(.caption)
+                        .foregroundStyle(.secondary)
+                }
+                
+                Spacer()
+
+                VStack(alignment: .trailing) {
+                    Text("\(game.score.unc) - \(game.score.opponent)")
+
+                    Text("\(game.isHomeGame ? "Home" : "Away")")
+                        .font(.caption)
+                        .foregroundStyle(.secondary)
+                }
+            }
         }
-        .padding()
+        .task {
+            await loadData()
+        }
+    }
+    
+    func loadData() async {
+        guard let url = URL(string: "http://localhost:8080/uncbasketball") else {
+            print("Invalid URL")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            if let decodedResponse = try? JSONDecoder().decode([BasketballGame].self, from: data) {
+                games = decodedResponse
+            }
+        } catch {
+            print("Invalid data")
+        }
     }
 }
 
