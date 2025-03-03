39c529fa58670547b9e743addf6485e006d8e574
Sam Shi <samrshi@outlook.com>
2025-03-02 22:55:55 -0500

    Add iTunes impl

--- iTunesSearch/iTunesSearch/ContentView.swift
+++ iTunesSearch/iTunesSearch/ContentView.swift
@@ -7,15 +7,46 @@
 
 import SwiftUI
 
+struct iTunesSearchResponse: Codable {
+    var results: [iTunesSearchResult]
+}
+
+struct iTunesSearchResult: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+}
+
 struct ContentView: View {
+    @State private var results = [iTunesSearchResult]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(results, id: \.trackId) { item in
+            VStack(alignment: .leading) {
+                Text(item.trackName).font(.headline)
+                Text(item.collectionName)
+            }
         }
-        .padding()
+        .task {
+            await loadData()
+        }
+    }
+    
+    func loadData() async {
+        guard let url = URL(string: "https://itunes.apple.com/search?term=the+beatles&entity=song") else {
+            print("Invalid URL")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            if let decodedResponse = try? JSONDecoder().decode(iTunesSearchResponse.self, from: data) {
+                results = decodedResponse.results
+            }
+        } catch {
+            print("Invalid data")
+        }
     }
 }
 
